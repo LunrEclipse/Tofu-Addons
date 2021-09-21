806c6d38160ebe233f748261c2855130fc7d5693
0XPYEX0 <50171612+0XPYEX0@users.noreply.github.com>
2021-09-21 17:39:11 +0800

    Update PhoenixDown.java

--- src/main/java/me/tofumc/tofumcaddon/events/PhoenixDown.java
+++ src/main/java/me/tofumc/tofumcaddon/events/PhoenixDown.java
@@ -51,6 +51,9 @@ public class PhoenixDown implements Listener {
         scheduler.scheduleSyncRepeatingTask(plugin, new Runnable() {
             public void run() {
                 for(Player p : Bukkit.getOnlinePlayers()){
+                    if (p.getInventory().getItemInMainHand() == null || myItem == null) {
+                        continue;
+                    } //可能出现空指针
                     if(p.getInventory().getItemInMainHand().getItemMeta().equals(myItem.getItemMeta()))
                         p.setFireTicks(25);
                 }
